001714b1deeae11950e5b396d456f7093da67bce
Bruno Moura <brunomoura.meta@gmail.com>
2025-03-12 09:01:17 -0300

    feat: cleanup on files and general organization.

--- main.c
+++ main.c
@@ -9,55 +9,20 @@
 #include "serial.h"
 #include "ht_protocol.h"
 #include "vlog.h"
+#include "helpers.h"
 
-pthread_mutex_t IOMutex;
-
-#define WRITE_TASK_NOTIFY(_format, ...)     \
-{                                           \
-    pthread_mutex_lock(&IOMutex);           \
-    VLOG_WRITE_TASK(_format, ##__VA_ARGS__);\
-    pthread_mutex_unlock(&IOMutex);         \
-}                                           \
-
-#define READ_TASK_NOTIFY(_format, ...)      \
-{                                           \
-    pthread_mutex_lock(&IOMutex);           \
-    VLOG_READ_TASK(_format, ##__VA_ARGS__); \
-    pthread_mutex_unlock(&IOMutex);         \
-}                                           \
-
-#define MAX_FOR(type) (1 << sizeof(type))
-
-#define SAFE_INCR(var) var = (var == MAX_FOR(var)) ? 1 : var + 1
-
-#define SEND_MSG(Payload, CmdId, Scope) \
-{                                                                                          \
-    char EncodedMessage[HT_PROTOCOL_CALC_ENCODED_SIZE(sizeof(Payload))];                   \
-    {Scope};                                                                               \
-    HT_PROTOCOL_InitMsg(&Message, 1, 2, CmdId, sizeof(Payload), (void *) &Payload, true);  \
-    HT_PROTOCOL_EncodeMsg(EncodedMessage, &Message);                                       \
-    serialWrite(Fd, EncodedMessage, HT_PROTOCOL_CALC_ENCODED_SIZE(sizeof(Payload)));       \
-    WRITE_TASK_NOTIFY("Wrote message for (%s) = %s", #Payload, EncodedMessage);            \
-}                                                                                          \
-
-#define RECEIVE_MSG(PayloadType, CmdId, Scope) \
-{                                                                           \
-        HT_PROTOCOL_Msg_t Message;                                          \
-        PayloadType Payload;                                                \
-        HT_PROTOCOL_DecodeMsg(&Message, &Payload, sizeof(Payload), Buffer); \
-        {Scope}                                                             \
-}                                                                           \
-
-const char *SerialPort = "/dev/pts/9";
+const char *SerialPort = "/dev/your_device";
 
 void ReadTask(void) {
 
+#define CMD_PAYLOAD_EXAMPLE_0_CMD_ID 0
     typedef struct __attribute__((packed)) {
         uint8  Data;
         uint16 Info;
         uint8  Signal;
     } CmdPayloadExample0_t;
 
+#define CMD_PAYLOAD_EXAMPLE_1_CMD_ID 1
     typedef struct __attribute__((packed)) {
         uint16 Data;
         uint16 Info;
@@ -65,9 +30,10 @@ void ReadTask(void) {
     } CmdPayloadExample1_t;
 
 
+#define CMD_PAYLOAD_EXAMPLE_2_CMD_ID 2
     typedef struct __attribute__((packed)) {
-        char   Data[20];
-        double Signal;
+        char  Data[20];
+        float Signal;
     } CmdPayloadExample2_t;
 
     int32 bytes_read = 0;
@@ -80,18 +46,24 @@ void ReadTask(void) {
         bytes_read = serialRead(Fd, Buffer, HT_HW_START_CHAR, HT_HW_END_CHAR, SERIAL_INCLUDE_MARKETS, HT_MAX_MESSAGE_BYTES);
 
         if (bytes_read <= 0) {
-            // READ_TASK_NOTIFY("Error reading serial\n");
+            // READ_TASK_NOTIFY(No bytes read on serial device\n");
         } else {
             switch(HT_PROTOCOL_GetCmdId(Buffer)) {
-                case 0: {
-                    RECEIVE_MSG(CmdPayloadExample0_t, 0, {
+                case CMD_PAYLOAD_EXAMPLE_0_CMD_ID: {
+                    RECEIVE_MSG(CmdPayloadExample0_t, CMD_PAYLOAD_EXAMPLE_0_CMD_ID, {
                         READ_TASK_NOTIFY("Payload0 = (%u, %u, %u)\n", Payload.Data, Payload.Info, Payload.Signal);
                     });
                     break;
                 }
-                case 1: {
-                    RECEIVE_MSG(CmdPayloadExample1_t, 1, {
+                case CMD_PAYLOAD_EXAMPLE_1_CMD_ID: {
+                    RECEIVE_MSG(CmdPayloadExample1_t, CMD_PAYLOAD_EXAMPLE_1_CMD_ID, {
                         READ_TASK_NOTIFY("Payload1 = (%u, %u, %lf)\n", Payload.Data, Payload.Info, Payload.Signal);
+                    });
+                    break;
+                }
+                case CMD_PAYLOAD_EXAMPLE_2_CMD_ID: {
+                    RECEIVE_MSG(CmdPayloadExample2_t, CMD_PAYLOAD_EXAMPLE_2_CMD_ID, {
+                        READ_TASK_NOTIFY("Payload2 = (%s, %f)\n", Payload.Data, Payload.Signal);
                     });
                     break;
                 }
@@ -106,7 +78,7 @@ void ReadTask(void) {
 
 void WriteTask(void) {
 
-    #define TLM_PAYLOAD_EXAMPLE_0_CMD_ID 0
+#define TLM_PAYLOAD_EXAMPLE_0_CMD_ID 0
     typedef struct {
         uint8  Info;
         uint8  Data;
@@ -114,12 +86,12 @@ void WriteTask(void) {
         float  Pressure;
     } TlmPayloadExample0_t;
 
-    #define TLM_PAYLOAD_EXAMPLE_1_CMD_ID 1
+#define TLM_PAYLOAD_EXAMPLE_1_CMD_ID 1
     typedef struct {
         double Measure;
     } TlmPayloadExample1_t;
 
-    #define TLM_PAYLOAD_EXAMPLE_2_CMD_ID 2
+#define TLM_PAYLOAD_EXAMPLE_2_CMD_ID 2
     typedef struct {
         uint8 ControlBits;
     } TlmPayloadExample2_t;
